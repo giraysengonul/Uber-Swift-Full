de5484c69b19d27ebb3c5cf65c9ca70bc1dfd6f0
hakkicansengonul <coder.9734@gmail.com>
2022-08-27 12:35:47 +0300

    Setup

---
+++ Uber-Swift-Full/Controller/LoginController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginController.swift
+//  Uber-Swift-Full
+//
+//  Created by hakkı can şengönül on 27.08.2022.
+//
+
+import UIKit
+class LoginController: UIViewController {
+     // MARK: - Properties
+    
+     // MARK: - Lifecyle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+ // MARK: - Helpers
+extension LoginController{
+    private func style(){
+        view.backgroundColor = .red
+    }
+    private func layout(){
+        
+    }
+}
